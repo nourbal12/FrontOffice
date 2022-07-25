7f75b5b290caa23e320a61d0df1e939719212d34
Balti Nour <nour.balti@etudiant-fst.utm.tn>
2022-07-25 17:36:50 +0200

    up

--- src/app/manage-blogs/add-blog/add-blog.component.ts
+++ src/app/manage-blogs/add-blog/add-blog.component.ts
@@ -33,7 +33,7 @@ export class AddBlogComponent implements OnInit {
       title: new FormControl(this.update?this.blog.title:'',[Validators.required]),
       author: new FormControl(this.user.name,[Validators.required]),
       content: new FormControl(this.update?this.blog.content:'',[Validators.required]),
-      author_id: new FormControl(this.user.id),
+      authorId: new FormControl(this.user.id),
       upvote: new FormControl(this.update?this.blog.upvote:0),
       downvote: new FormControl(this.update?this.blog.downvote:0),
       
@@ -43,7 +43,7 @@ export class AddBlogComponent implements OnInit {
 
   addBlog() {
     this.form.value.author=this.user.name;
-    this.form.value.author_id=this.user.id;
+    this.form.value.authorId=this.user.id;
     this.update ? this.blogService.updateArticle(this.blog.id, this.form.value).subscribe() : this.blogService.addArticle(this.form.value).subscribe();
     this.update?this.removeUpdateModal():this.router.navigate(['list']);
     
